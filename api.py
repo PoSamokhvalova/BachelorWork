f2dac063e6afb8a64cbf9a219c81abfd77c40c6b
Polyna Samokhvalova <polynasamokhvalova89@gmail.com>
2019-05-28 22:15:26 +0300

    final update

--- api.py
+++ api.py
@@ -9,23 +9,20 @@ app = Flask(__name__)
 @app.route('/')
 def show_main_page():
 
-    clist = ["AdMob","Advertising whale",'AET','AEIndustries','Agile Mobile','ALT Services','Ammo','AppGen','Appss','BCA','CodiProf','Dextra','Doom','Dukes','GenerationM','GenMob','KeyMarketing','MatriX','MobileKeyServices','Mobiller','Mobistar','Mobonobe','Mobster','Newlight','Neostar','Optiserv','Place4','Proddev','Production melon','Prodster','Quantum mobile','QXZ','RatioAdv','Refcord','RingRang','ServTech','Simone Well','Victory Development','VMessage','XTF sms']
+    clist = ['AE','BE','BH','CA','CH','IE','SA']
 
-    return render_template('text.html', companies=clist)
+    return render_template('text.html', countries=clist)
 
 
 @app.route('/price', methods=['POST'])
 def calculate_price():
-
-    # вместо рандом числа передать параметер из запроса другой функции
-
-    selected_country = "AE"
-
+    selected_country = request.json['country']
+    print(selected_country)
     data = {
         'price': data_helpers.calculate_neo4j_price(country=selected_country)
     }
     js = json.dumps(data)
-    command= 'select dfgv -> njj []'
+
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
