392602ad82074b773d5c4fe51b9eac3e7ac14f99
Kateryna Nesvit <>
2019-05-28 20:58:17 +0300

    added "do not track temporary files"

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, Response
 import json
-from random import randint
 import data_helpers
 
 app = Flask(__name__)
@@ -17,15 +16,13 @@ def show_main_page():
 @app.route('/price', methods=['POST'])
 def calculate_price():
 
-    # вместо рандом числа передать параметер из запроса другой функции
-
     selected_country = "AE"
 
     data = {
         'price': data_helpers.calculate_neo4j_price(country=selected_country)
     }
     js = json.dumps(data)
-    command= 'select dfgv -> njj []'
+
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
